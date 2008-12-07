0f5fb07b81f52a38fc1336c655d29ff83605824f
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2008-12-07 16:42:48 +0000

    classy_tee! sets up has_one_accessors assuming that "#{class_name}Extensions" exists

--- init.rb
+++ init.rb
@@ -1,3 +1,5 @@
 unless defined?(HasOneAccessor)
   require File.dirname(__FILE__) + "/vendor/has_one_accessor/init"
 end
+require File.dirname(__FILE__) + "/lib/classy_tee"
+ActiveRecord::Base.extend ClassyTee::ActsMethods
--- lib/classy_tee.rb
+++ lib/classy_tee.rb
@@ -1,1 +1,14 @@
-# ClassyTee
+module ClassyTee
+  module ActsMethods
+    def classy_tee!
+      foreign_key = "#{self.name.underscore}_id"
+      association = "#{self.name}Extension".constantize
+      association_key = association.name.underscore.to_sym
+      cti_columns = association.column_names - ["id", foreign_key, "created_at", "updated_at"]
+      
+      has_one association_key, :foreign_key => foreign_key
+      has_one_accessor association_key, cti_columns, :prefix => false
+      default_scope(:include => association_key) if self.respond_to?(:default_scope)
+    end
+  end
+end
